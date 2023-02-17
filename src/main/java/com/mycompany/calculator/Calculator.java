dc214aa321ecb84c0393f853633d900a6f1be7a9
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2023-02-17 11:24:19 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author INDRAJITH
+ */
+ 
+public class Calculator {
+
+public static void main(String[] args) {  
+    new Frametext();  
+}
+    
+}
